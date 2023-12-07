4dfe44411eea468ca3941078f0e138127229631d
Joao.Macosso <joaoclaudiomacosso@gmail.com>
2023-12-07 02:46:00 +0100

    Create xttab.R

---
+++ R/xttab.R
@@ -0,0 +1,13 @@
+
+
+
+xttab <- function(data,
+                  variables = NULL,
+                  id = NULL,
+                  t = NULL,
+                  na.rm = FALSE,
+                  return.data.frame = FALSE,
+                  dec = 3){
+  # Work in progress
+  return(head(data))
+}
